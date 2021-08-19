2c8359df930b0548671d8f066fc47cce63224dd6
Eduardo Szeckir <eszeckirjr@gmail.com>
2021-08-18 20:54:09 -0700

    Adding experience page

---
+++ src/components/experience/WorkTimelineElement.tsx
@@ -0,0 +1,48 @@
+import { InfoOutlineIcon } from "@chakra-ui/icons";
+import { Box, Heading, HStack, IconButton, Text, useDisclosure } from "@chakra-ui/react";
+import { RiMentalHealthLine } from "react-icons/ri";
+import { VerticalTimelineElement } from "react-vertical-timeline-component";
+import { Experience } from "../../docs/ExperienceData";
+import ExperienceModal from "./ExperienceModal";
+
+export default function WorkTimelineElement({ title, subtitle, description, color, range }: Experience){
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    return (
+        <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            contentStyle={{ background: color, color: '#fff' }}
+            contentArrowStyle={{ borderRight: ('7px solid  '+color) }}
+            date={range}
+            iconStyle={{ background: color, color: '#fff' }}
+            icon={<RiMentalHealthLine />}
+            onTimelineElementClick={onOpen}
+            style={ {cursor:"pointer"} }
+        >
+            <HStack>
+                <Box flexGrow={10}>
+                    <Heading
+                        className="vertical-timeline-element-title"
+                        as="h3"
+                        fontWeight="bold"
+                        size="md"
+                    >{title}</Heading>
+                    <Text
+                        className="vertical-timeline-element-subtitle"
+                        as="h4"
+                        fontSize="1.25em"
+                    >{subtitle}</Text>
+                </Box>
+                <IconButton
+                    isRound
+                    bgColor={color}
+                    filter="contrast(130%)"
+                    aria-label="More info"
+                    onClick={onOpen}
+                    icon={<InfoOutlineIcon />}
+                    shadow="lg"
+                />
+            </HStack>
+            <ExperienceModal isOpen={isOpen} onClose={onClose} experience={{ title, subtitle, description, color, range }} />
+        </VerticalTimelineElement>
+    );
+}
---
+++ src/docs/ExperienceData.tsx
@@ -0,0 +1,52 @@
+export type Experience = {
+    title: string;
+    subtitle: string;
+    range: string;
+    description: string[];
+    color: string;
+}
+
+export const coopExperienceData: Experience[] = [
+    {
+        title: "Software Engineering Intern",
+        subtitle: "Centre for Addiction and Mental Health (CAMH)",
+        range: "Sep. 2020 - Dec. 2020",
+        description: [
+            "For my first co-op, I worked at Centre for Addiction and Mental Health (CAMH), the largest mental health hospital in Canada. I worked entirely remotely due to the COVID-19 pandemic. There, I worked on developing an interactive web based application from scratch to better support the creation of health care plans in a health-specific data standard.",
+            "Worked with Python and Dash for the generation of interactive interfaces. The second version was made using HTML, CSS, and JavaScript. The application runs on a Node.js/Express.js server and it has a MongoDB database for care plan storage."
+        ],
+        color: "#97266D"
+    }
+]
+
+export const otherExperienceData: Experience[] = [
+    {
+        title: "Campus Tour Guide",
+        subtitle: "University of Victoria",
+        range: "Nov. 2019 - Present",
+        description: [
+            "Representing UVic as a Campus Tour Guide by showing the campus to prospective students and their families.",
+            "Acquired skills such as public speaking and interpersonal relations.",
+        ],
+        color: "#ffaf00"
+    },
+    {
+        title: "Digitization Assistant",
+        subtitle: "Univeristy of Victoria - Libraries",
+        range: "Sep. 2019 - Apr. 2020",
+        description: [
+            "Scanning old BC Gazettes with the intent of uploading them into the Internet Archive.",
+            "Learned how to use new equipament/software very fast, while learning how to work on an office environment for hours a day."
+        ],
+        color: "gray"
+    },
+    {
+        title: "Student Painter",
+        subtitle: "Scholars Edge Painting",
+        range: "May. 2019 - Sep. 2019",
+        description: [
+            "Working as a student painter over the summer. Worked with clients (i.e. answering questions and handling any concerns they might have had) and worked outdoors. Also learned to be extremely flexible, since our time/location was usually given 30min before the job started."
+        ],
+        color: "#ff6600"
+    }
+]
